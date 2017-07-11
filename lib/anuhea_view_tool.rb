4801c6b61f28e83bb4588cf7c958ceaf7570995d
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:42:15 +0000

    Dev tool initial commit

---
+++ lib/anuhea_view_tool.rb
@@ -0,0 +1,6 @@
+require "anuhea_view_tool/version"
+require "anuhea_view_tool/renderer"
+
+module AnuheaViewTool
+  # Your code goes here...
+end
---
+++ lib/anuhea_view_tool/version.rb
@@ -0,0 +1,3 @@
+module AnuheaViewTool
+  VERSION = "0.1.0"
+end
